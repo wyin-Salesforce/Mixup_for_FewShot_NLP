f457099494ab2b7339c0883a53469d789c3ade5b
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-07 16:09:26 -0700

    coding

--- command_lines.sh
+++ command_lines.sh
@@ -1,87 +1,6 @@
-export domain='utility'
+export domain='work'
 export BETATIMES=15
 
-# CUDA_VISIBLE_DEVICES=0 python -u train_CLINC150.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 50 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 5 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 20 \
-#     --seed 42 \
-#     --DomainName $domain \
-#     --kshot 3 \
-#     --beta_sampling_times 1 > log.seed.42.txt 2>&1 &
-#
-# CUDA_VISIBLE_DEVICES=1 python -u train_CLINC150.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 50 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 5 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 20 \
-#     --seed 16 \
-#     --DomainName $domain \
-#     --kshot 3 \
-#     --beta_sampling_times 1 > log.seed.16.txt 2>&1 &
-#
-# CUDA_VISIBLE_DEVICES=2 python -u train_CLINC150.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 50 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 5 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 20 \
-#     --seed 32 \
-#     --DomainName $domain \
-#     --kshot 3 \
-#     --beta_sampling_times 1 > log.seed.32.txt 2>&1 &
-#
-# CUDA_VISIBLE_DEVICES=3 python -u train_CLINC150.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 50 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 5 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 20 \
-#     --seed 64 \
-#     --DomainName $domain \
-#     --kshot 3 \
-#     --beta_sampling_times 1 > log.seed.64.txt 2>&1 &
-#
-# CUDA_VISIBLE_DEVICES=4 python -u train_CLINC150.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 50 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 5 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 20 \
-#     --seed 128 \
-#     --DomainName $domain \
-#     --kshot 3 \
-#     --beta_sampling_times 1 > log.seed.128.txt 2>&1 &
-
-#w/ mixup
 CUDA_VISIBLE_DEVICES=0 python -u train_CLINC150.py \
     --task_name rte \
     --do_train \
@@ -91,13 +10,12 @@ CUDA_VISIBLE_DEVICES=0 python -u train_CLINC150.py \
     --output_dir '' \
     --train_batch_size 5 \
     --eval_batch_size 32 \
-    --learning_rate 1e-6 \
+    --learning_rate 5e-6 \
     --max_seq_length 20 \
     --seed 42 \
     --DomainName $domain \
     --kshot 3 \
-    --use_mixup\
-    --beta_sampling_times $BETATIMES > log.seed.42.txt 2>&1 &
+    --beta_sampling_times 1 > log.seed.42.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=1 python -u train_CLINC150.py \
     --task_name rte \
@@ -108,13 +26,12 @@ CUDA_VISIBLE_DEVICES=1 python -u train_CLINC150.py \
     --output_dir '' \
     --train_batch_size 5 \
     --eval_batch_size 32 \
-    --learning_rate 1e-6 \
+    --learning_rate 5e-6 \
     --max_seq_length 20 \
     --seed 16 \
     --DomainName $domain \
     --kshot 3 \
-    --use_mixup\
-    --beta_sampling_times $BETATIMES > log.seed.16.txt 2>&1 &
+    --beta_sampling_times 1 > log.seed.16.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=2 python -u train_CLINC150.py \
     --task_name rte \
@@ -125,13 +42,12 @@ CUDA_VISIBLE_DEVICES=2 python -u train_CLINC150.py \
     --output_dir '' \
     --train_batch_size 5 \
     --eval_batch_size 32 \
-    --learning_rate 1e-6 \
+    --learning_rate 5e-6 \
     --max_seq_length 20 \
     --seed 32 \
     --DomainName $domain \
     --kshot 3 \
-    --use_mixup\
-    --beta_sampling_times $BETATIMES > log.seed.32.txt 2>&1 &
+    --beta_sampling_times 1 > log.seed.32.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=3 python -u train_CLINC150.py \
     --task_name rte \
@@ -142,13 +58,12 @@ CUDA_VISIBLE_DEVICES=3 python -u train_CLINC150.py \
     --output_dir '' \
     --train_batch_size 5 \
     --eval_batch_size 32 \
-    --learning_rate 1e-6 \
+    --learning_rate 5e-6 \
     --max_seq_length 20 \
     --seed 64 \
     --DomainName $domain \
     --kshot 3 \
-    --use_mixup\
-    --beta_sampling_times $BETATIMES > log.seed.64.txt 2>&1 &
+    --beta_sampling_times 1 > log.seed.64.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=4 python -u train_CLINC150.py \
     --task_name rte \
@@ -159,10 +74,95 @@ CUDA_VISIBLE_DEVICES=4 python -u train_CLINC150.py \
     --output_dir '' \
     --train_batch_size 5 \
     --eval_batch_size 32 \
-    --learning_rate 1e-6 \
+    --learning_rate 5e-6 \
     --max_seq_length 20 \
     --seed 128 \
     --DomainName $domain \
     --kshot 3 \
-    --use_mixup\
-    --beta_sampling_times $BETATIMES > log.seed.128.txt 2>&1 &
+    --beta_sampling_times 1 > log.seed.128.txt 2>&1 &
+
+#w/ mixup
+# CUDA_VISIBLE_DEVICES=0 python -u train_CLINC150.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 50 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 5 \
+#     --eval_batch_size 32 \
+#     --learning_rate 1e-6 \
+#     --max_seq_length 20 \
+#     --seed 42 \
+#     --DomainName $domain \
+#     --kshot 3 \
+#     --use_mixup\
+#     --beta_sampling_times $BETATIMES > log.seed.42.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=1 python -u train_CLINC150.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 50 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 5 \
+#     --eval_batch_size 32 \
+#     --learning_rate 1e-6 \
+#     --max_seq_length 20 \
+#     --seed 16 \
+#     --DomainName $domain \
+#     --kshot 3 \
+#     --use_mixup\
+#     --beta_sampling_times $BETATIMES > log.seed.16.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=2 python -u train_CLINC150.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 50 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 5 \
+#     --eval_batch_size 32 \
+#     --learning_rate 1e-6 \
+#     --max_seq_length 20 \
+#     --seed 32 \
+#     --DomainName $domain \
+#     --kshot 3 \
+#     --use_mixup\
+#     --beta_sampling_times $BETATIMES > log.seed.32.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=3 python -u train_CLINC150.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 50 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 5 \
+#     --eval_batch_size 32 \
+#     --learning_rate 1e-6 \
+#     --max_seq_length 20 \
+#     --seed 64 \
+#     --DomainName $domain \
+#     --kshot 3 \
+#     --use_mixup\
+#     --beta_sampling_times $BETATIMES > log.seed.64.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=4 python -u train_CLINC150.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 50 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 5 \
+#     --eval_batch_size 32 \
+#     --learning_rate 1e-6 \
+#     --max_seq_length 20 \
+#     --seed 128 \
+#     --DomainName $domain \
+#     --kshot 3 \
+#     --use_mixup\
+#     --beta_sampling_times $BETATIMES > log.seed.128.txt 2>&1 &
