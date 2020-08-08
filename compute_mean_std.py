3251b23151811efa843088ec9d404a35a62c2353
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-07 17:59:26 -0700

    coding

--- compute_mean_std.py
+++ compute_mean_std.py
@@ -2,13 +2,8 @@ import statistics
 
 
 
-
-
-
-
-
 # initializing list
-test_list = [87.11, 89.11, 90.88, 86.88, 87.55]
+test_list = [81.33, 85.33, 85.11, 82.88, 73.55]
 print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)
