daeb38d397f1bf4fd5c878b7d8fd9cdebef80436
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-12 16:10:13 -0700

    coding

--- command_lines_RTE.sh
+++ command_lines_RTE.sh
@@ -1,83 +1,84 @@
 export BETATIMES=15
-export SHOT=3
+export SHOT=5
+export BATCHSIZE=5
 
 
 
-# CUDA_VISIBLE_DEVICES=0 python -u train_RTE.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 100 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 3 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 128 \
-#     --seed 42 \
-#     --kshot $SHOT \
-#     --beta_sampling_times 1 > log.seed.42.txt 2>&1 &
-#
-# CUDA_VISIBLE_DEVICES=1 python -u train_RTE.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 100 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 3 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 128 \
-#     --seed 16 \
-#     --kshot $SHOT \
-#     --beta_sampling_times 1 > log.seed.16.txt 2>&1 &
-#
-# CUDA_VISIBLE_DEVICES=2 python -u train_RTE.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 100 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 3 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 128 \
-#     --seed 32 \
-#     --kshot $SHOT \
-#     --beta_sampling_times 1 > log.seed.32.txt 2>&1 &
-#
-# CUDA_VISIBLE_DEVICES=3 python -u train_RTE.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 100 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 3 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 128 \
-#     --seed 64 \
-#     --kshot $SHOT \
-#     --beta_sampling_times 1 > log.seed.64.txt 2>&1 &
-#
-#
-# CUDA_VISIBLE_DEVICES=4 python -u train_RTE.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs 100 \
-#     --data_dir '' \
-#     --output_dir '' \
-#     --train_batch_size 3 \
-#     --eval_batch_size 32 \
-#     --learning_rate 5e-6 \
-#     --max_seq_length 128 \
-#     --seed 128 \
-#     --kshot $SHOT \
-#     --beta_sampling_times 1 > log.seed.128.txt 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python -u train_RTE.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs 100 \
+    --data_dir '' \
+    --output_dir '' \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 32 \
+    --learning_rate 5e-6 \
+    --max_seq_length 128 \
+    --seed 42 \
+    --kshot $SHOT \
+    --beta_sampling_times 1 > log.seed.42.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=1 python -u train_RTE.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs 100 \
+    --data_dir '' \
+    --output_dir '' \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 32 \
+    --learning_rate 5e-6 \
+    --max_seq_length 128 \
+    --seed 16 \
+    --kshot $SHOT \
+    --beta_sampling_times 1 > log.seed.16.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=2 python -u train_RTE.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs 100 \
+    --data_dir '' \
+    --output_dir '' \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 32 \
+    --learning_rate 5e-6 \
+    --max_seq_length 128 \
+    --seed 32 \
+    --kshot $SHOT \
+    --beta_sampling_times 1 > log.seed.32.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=3 python -u train_RTE.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs 100 \
+    --data_dir '' \
+    --output_dir '' \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 32 \
+    --learning_rate 5e-6 \
+    --max_seq_length 128 \
+    --seed 64 \
+    --kshot $SHOT \
+    --beta_sampling_times 1 > log.seed.64.txt 2>&1 &
+
+
+CUDA_VISIBLE_DEVICES=4 python -u train_RTE.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs 100 \
+    --data_dir '' \
+    --output_dir '' \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 32 \
+    --learning_rate 5e-6 \
+    --max_seq_length 128 \
+    --seed 128 \
+    --kshot $SHOT \
+    --beta_sampling_times 1 > log.seed.128.txt 2>&1 &
 
 
 #w/ mixup
@@ -88,7 +89,7 @@ CUDA_VISIBLE_DEVICES=0 python -u train_RTE.py \
     --num_train_epochs 100 \
     --data_dir '' \
     --output_dir '' \
-    --train_batch_size 3 \
+    --train_batch_size $BATCHSIZE \
     --eval_batch_size 32 \
     --learning_rate 1e-6 \
     --max_seq_length 128 \
@@ -104,7 +105,7 @@ CUDA_VISIBLE_DEVICES=1 python -u train_RTE.py \
     --num_train_epochs 100 \
     --data_dir '' \
     --output_dir '' \
-    --train_batch_size 3 \
+    --train_batch_size $BATCHSIZE \
     --eval_batch_size 32 \
     --learning_rate 1e-6 \
     --max_seq_length 128 \
@@ -120,7 +121,7 @@ CUDA_VISIBLE_DEVICES=2 python -u train_RTE.py \
     --num_train_epochs 100 \
     --data_dir '' \
     --output_dir '' \
-    --train_batch_size 3 \
+    --train_batch_size $BATCHSIZE \
     --eval_batch_size 32 \
     --learning_rate 1e-6 \
     --max_seq_length 128 \
@@ -136,7 +137,7 @@ CUDA_VISIBLE_DEVICES=3 python -u train_RTE.py \
     --num_train_epochs 100 \
     --data_dir '' \
     --output_dir '' \
-    --train_batch_size 3 \
+    --train_batch_size $BATCHSIZE \
     --eval_batch_size 32 \
     --learning_rate 1e-6 \
     --max_seq_length 128 \
@@ -152,7 +153,7 @@ CUDA_VISIBLE_DEVICES=4 python -u train_RTE.py \
     --num_train_epochs 100 \
     --data_dir '' \
     --output_dir '' \
-    --train_batch_size 3 \
+    --train_batch_size $BATCHSIZE \
     --eval_batch_size 32 \
     --learning_rate 1e-6 \
     --max_seq_length 128 \
--- compute_mean_std.py
+++ compute_mean_std.py
@@ -5,9 +5,8 @@ import statistics
 
 
 
-
 # initializing list
-test_list = [86.22, 88, 87.11, 83.77, 82.88]
+test_list = [50.31, 50.75, 50.65, 52.18, 49.64]
 print('sum:', sum(test_list))
 average = round(sum(test_list)/len(test_list), 2)
 res = round(statistics.pstdev(test_list),2)
