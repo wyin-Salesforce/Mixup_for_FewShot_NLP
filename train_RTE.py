495fe224e366e6c06bd18d679fdb90f68c6fb0bd
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-12 15:44:40 -0700

    coding

--- train_RTE.py
+++ train_RTE.py
@@ -823,7 +823,7 @@ if __name__ == "__main__":
 
 '''
 mixup:
-CUDA_VISIBLE_DEVICES=6 python -u train_CLINC150.py --task_name rte --do_train --do_lower_case --num_train_epochs 100 --data_dir '' --output_dir '' --train_batch_size 5 --eval_batch_size 32 --learning_rate 1e-6 --max_seq_length 20 --seed 42 --DomainName 'banking' --kshot 3 --use_mixup --beta_sampling_times 10
+CUDA_VISIBLE_DEVICES=6 python -u train_RTE.py --task_name rte --do_train --do_lower_case --num_train_epochs 100 --data_dir '' --output_dir '' --train_batch_size 3 --eval_batch_size 32 --learning_rate 1e-6 --max_seq_length 128 --seed 42 --kshot 3 --use_mixup --beta_sampling_times 10
 
 no mixup:
 CUDA_VISIBLE_DEVICES=5 python -u train_RTE.py --task_name rte --do_train --do_lower_case --num_train_epochs 100 --data_dir '' --output_dir '' --train_batch_size 3 --eval_batch_size 32 --learning_rate 1e-6 --max_seq_length 128 --seed 42 --kshot 3 --beta_sampling_times 1
