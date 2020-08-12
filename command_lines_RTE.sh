27d4c3d18f0ad6691693d5e62bda475c1b29cc72
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-12 15:54:10 -0700

    coding

--- command_lines_RTE.sh
+++ command_lines_RTE.sh
@@ -3,6 +3,84 @@ export SHOT=3
 
 
 
+# CUDA_VISIBLE_DEVICES=0 python -u train_RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 100 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 3 \
+#     --eval_batch_size 32 \
+#     --learning_rate 5e-6 \
+#     --max_seq_length 128 \
+#     --seed 42 \
+#     --kshot $SHOT \
+#     --beta_sampling_times 1 > log.seed.42.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=1 python -u train_RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 100 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 3 \
+#     --eval_batch_size 32 \
+#     --learning_rate 5e-6 \
+#     --max_seq_length 128 \
+#     --seed 16 \
+#     --kshot $SHOT \
+#     --beta_sampling_times 1 > log.seed.16.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=2 python -u train_RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 100 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 3 \
+#     --eval_batch_size 32 \
+#     --learning_rate 5e-6 \
+#     --max_seq_length 128 \
+#     --seed 32 \
+#     --kshot $SHOT \
+#     --beta_sampling_times 1 > log.seed.32.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=3 python -u train_RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 100 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 3 \
+#     --eval_batch_size 32 \
+#     --learning_rate 5e-6 \
+#     --max_seq_length 128 \
+#     --seed 64 \
+#     --kshot $SHOT \
+#     --beta_sampling_times 1 > log.seed.64.txt 2>&1 &
+#
+#
+# CUDA_VISIBLE_DEVICES=4 python -u train_RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs 100 \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size 3 \
+#     --eval_batch_size 32 \
+#     --learning_rate 5e-6 \
+#     --max_seq_length 128 \
+#     --seed 128 \
+#     --kshot $SHOT \
+#     --beta_sampling_times 1 > log.seed.128.txt 2>&1 &
+
+
+#w/ mixup
 CUDA_VISIBLE_DEVICES=0 python -u train_RTE.py \
     --task_name rte \
     --do_train \
@@ -12,11 +90,12 @@ CUDA_VISIBLE_DEVICES=0 python -u train_RTE.py \
     --output_dir '' \
     --train_batch_size 3 \
     --eval_batch_size 32 \
-    --learning_rate 5e-6 \
+    --learning_rate 1e-6 \
     --max_seq_length 128 \
     --seed 42 \
     --kshot $SHOT \
-    --beta_sampling_times 1 > log.seed.42.txt 2>&1 &
+    --use_mixup\
+    --beta_sampling_times $BETATIMES > log.mixup.42.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=1 python -u train_RTE.py \
     --task_name rte \
@@ -27,11 +106,12 @@ CUDA_VISIBLE_DEVICES=1 python -u train_RTE.py \
     --output_dir '' \
     --train_batch_size 3 \
     --eval_batch_size 32 \
-    --learning_rate 5e-6 \
+    --learning_rate 1e-6 \
     --max_seq_length 128 \
     --seed 16 \
     --kshot $SHOT \
-    --beta_sampling_times 1 > log.seed.16.txt 2>&1 &
+    --use_mixup\
+    --beta_sampling_times $BETATIMES > log.mixup.16.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=2 python -u train_RTE.py \
     --task_name rte \
@@ -42,11 +122,12 @@ CUDA_VISIBLE_DEVICES=2 python -u train_RTE.py \
     --output_dir '' \
     --train_batch_size 3 \
     --eval_batch_size 32 \
-    --learning_rate 5e-6 \
+    --learning_rate 1e-6 \
     --max_seq_length 128 \
     --seed 32 \
     --kshot $SHOT \
-    --beta_sampling_times 1 > log.seed.32.txt 2>&1 &
+    --use_mixup\
+    --beta_sampling_times $BETATIMES > log.mixup.32.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=3 python -u train_RTE.py \
     --task_name rte \
@@ -57,12 +138,12 @@ CUDA_VISIBLE_DEVICES=3 python -u train_RTE.py \
     --output_dir '' \
     --train_batch_size 3 \
     --eval_batch_size 32 \
-    --learning_rate 5e-6 \
+    --learning_rate 1e-6 \
     --max_seq_length 128 \
     --seed 64 \
     --kshot $SHOT \
-    --beta_sampling_times 1 > log.seed.64.txt 2>&1 &
-
+    --use_mixup\
+    --beta_sampling_times $BETATIMES > log.mixup.64.txt 2>&1 &
 
 CUDA_VISIBLE_DEVICES=4 python -u train_RTE.py \
     --task_name rte \
@@ -73,8 +154,9 @@ CUDA_VISIBLE_DEVICES=4 python -u train_RTE.py \
     --output_dir '' \
     --train_batch_size 3 \
     --eval_batch_size 32 \
-    --learning_rate 5e-6 \
+    --learning_rate 1e-6 \
     --max_seq_length 128 \
     --seed 128 \
     --kshot $SHOT \
-    --beta_sampling_times 1 > log.seed.128.txt 2>&1 &
+    --use_mixup\
+    --beta_sampling_times $BETATIMES > log.mixup.128.txt 2>&1 &
