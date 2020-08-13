8aa90525b494b88cb8b279de30e2ee26a3946395
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-13 16:38:59 -0700

    coding

--- command_lines_MRPC.sh
+++ command_lines_MRPC.sh
@@ -20,20 +20,20 @@ CUDA_VISIBLE_DEVICES=0 python -u train_MRPC.py \
     --kshot $SHOT \
     --beta_sampling_times 1 > log.seed.42.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u train_MRPC.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --data_dir '' \
-    --output_dir '' \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
-    --learning_rate $LR \
-    --max_seq_length 128 \
-    --seed 16 \
-    --kshot $SHOT \
-    --beta_sampling_times 1 > log.seed.16.txt 2>&1 &
+# CUDA_VISIBLE_DEVICES=1 python -u train_MRPC.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --data_dir '' \
+#     --output_dir '' \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 32 \
+#     --learning_rate $LR \
+#     --max_seq_length 128 \
+#     --seed 16 \
+#     --kshot $SHOT \
+#     --beta_sampling_times 1 > log.seed.16.txt 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=2 python -u train_MRPC.py \
 #     --task_name rte \
@@ -50,7 +50,7 @@ CUDA_VISIBLE_DEVICES=1 python -u train_MRPC.py \
 #     --kshot $SHOT \
 #     --beta_sampling_times 1 > log.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=0 python -u train_MRPC.py \
+CUDA_VISIBLE_DEVICES=2 python -u train_MRPC.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
