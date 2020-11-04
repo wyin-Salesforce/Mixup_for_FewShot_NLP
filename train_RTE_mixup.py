5d36fcdc010b24f11db8c821b86cdc84494d0487
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-04 13:52:57 -0800

    coding

--- train_RTE_mixup.py
+++ train_RTE_mixup.py
@@ -229,7 +229,7 @@ class RteProcessor(DataProcessor):
         readfile.close()
         print('loaded  entail size:', len(examples_entail), 'non-entail size:', len(examples_non_entail))
         '''sampling'''
-        if k_shot > 99999:
+        if k_shot  == 0:
             return examples_entail+examples_non_entail
         else:
             sampled_examples = random.sample(examples_entail, k_shot)+random.sample(examples_non_entail, k_shot)
@@ -624,7 +624,7 @@ def main():
 
     model = RobertaForSequenceClassification(num_labels)
     tokenizer = RobertaTokenizer.from_pretrained(pretrain_model_dir, do_lower_case=args.do_lower_case)
-    model.load_state_dict(torch.load('/export/home/Dataset/BERT_pretrained_mine/mixup_wenpeng/kshot_3_seed_16_RTE_acc_0.5523465703971119.pt'))
+    # model.load_state_dict(torch.load('/export/home/Dataset/BERT_pretrained_mine/mixup_wenpeng/kshot_3_seed_16_RTE_acc_0.5523465703971119.pt'))
     model.to(device)
 
     param_optimizer = list(model.named_parameters())
@@ -826,7 +826,7 @@ if __name__ == "__main__":
 
 '''
 mixup:
-CUDA_VISIBLE_DEVICES=4 python -u train_RTE.py --task_name rte --do_train --do_lower_case --num_train_epochs 20 --data_dir '' --output_dir '' --train_batch_size 5 --eval_batch_size 32 --learning_rate 1e-6 --max_seq_length 128 --seed 42 --kshot 100000 --use_mixup --beta_sampling_times 15
+CUDA_VISIBLE_DEVICES=4 python -u train_RTE.mixup.py --task_name rte --do_train --do_lower_case --num_train_epochs 20 --train_batch_size 5 --eval_batch_size 32 --learning_rate 1e-6 --max_seq_length 128 --seed 42 --kshot 0 --use_mixup --beta_sampling_times 15
 
 no mixup:
 CUDA_VISIBLE_DEVICES=5 python -u train_RTE.py --task_name rte --do_train --do_lower_case --num_train_epochs 100 --data_dir '' --output_dir '' --train_batch_size 3 --eval_batch_size 32 --learning_rate 1e-6 --max_seq_length 128 --seed 42 --kshot 3 --beta_sampling_times 1
