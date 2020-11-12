8c20c221e7554958512215b429cfc7d87e2e79ad
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-11 18:29:18 -0800

    coding

--- new/train.RTE.mixup.py
+++ new/train.RTE.mixup.py
@@ -686,8 +686,10 @@ def main():
 
                 for sample_i in range(args.beta_sampling_times):
                     lambda_vec = beta.rvs(0.4, 0.4, size=1)[0]
-                    print(epoch_i, step, sample_i, 'lambda_vec:', lambda_vec)
+                    # print(epoch_i, step, sample_i, 'lambda_vec:', lambda_vec)
                     loss = model(input_ids, input_mask, label_ids, lambda_vec, is_train=True, use_mixup=args.use_mixup)
+                    print(epoch_i, step, sample_i, 'lambda_vec:', lambda_vec, 'input_ids:', input_ids[0:5, :10])
+                    print('loss:', loss.item())
                     if n_gpu > 1:
                         loss = loss.mean() # mean() to average on multi-gpu.
                     if args.gradient_accumulation_steps > 1:
