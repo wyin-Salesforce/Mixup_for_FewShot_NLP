fb71a4a86261f640b5178e5824c0e3ec0232fd29
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-12 20:34:36 -0800

    coding

--- new/train.RTE.commands.sh
+++ new/train.RTE.commands.sh
@@ -5,6 +5,55 @@ export LEARNINGRATE=1e-6
 
 
 
+# CUDA_VISIBLE_DEVICES=0 python -u train.RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 32 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length 128 \
+#     --seed 42 \
+#     --kshot $SHOT > log.RTE.$SHOT.shot.seed.42.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=1 python -u train.RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 32 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length 128 \
+#     --seed 16 \
+#     --kshot $SHOT > log.RTE.$SHOT.shot.seed.16.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=2 python -u train.RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 32 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length 128 \
+#     --seed 32 \
+#     --kshot $SHOT > log.RTE.$SHOT.shot.seed.32.txt 2>&1 &
+#
+# CUDA_VISIBLE_DEVICES=3 python -u train.RTE.py \
+#     --task_name rte \
+#     --do_train \
+#     --do_lower_case \
+#     --num_train_epochs $EPOCHSIZE \
+#     --train_batch_size $BATCHSIZE \
+#     --eval_batch_size 32 \
+#     --learning_rate $LEARNINGRATE \
+#     --max_seq_length 128 \
+#     --seed 64 \
+#     --kshot $SHOT > log.RTE.$SHOT.shot.seed.64.txt 2>&1 &
+
+
 CUDA_VISIBLE_DEVICES=0 python -u train.RTE.py \
     --task_name rte \
     --do_train \
@@ -14,54 +63,5 @@ CUDA_VISIBLE_DEVICES=0 python -u train.RTE.py \
     --eval_batch_size 32 \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
-    --seed 42 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.42.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=1 python -u train.RTE.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 128 \
-    --seed 16 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.16.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=2 python -u train.RTE.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 128 \
-    --seed 32 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.32.txt 2>&1 &
-
-CUDA_VISIBLE_DEVICES=3 python -u train.RTE.py \
-    --task_name rte \
-    --do_train \
-    --do_lower_case \
-    --num_train_epochs $EPOCHSIZE \
-    --train_batch_size $BATCHSIZE \
-    --eval_batch_size 32 \
-    --learning_rate $LEARNINGRATE \
-    --max_seq_length 128 \
-    --seed 64 \
-    --kshot $SHOT > log.RTE.$SHOT.shot.seed.64.txt 2>&1 &
-
-
-# CUDA_VISIBLE_DEVICES=4 python -u train.RTE.py \
-#     --task_name rte \
-#     --do_train \
-#     --do_lower_case \
-#     --num_train_epochs $EPOCHSIZE \
-#     --train_batch_size $BATCHSIZE \
-#     --eval_batch_size 32 \
-#     --learning_rate $LEARNINGRATE \
-#     --max_seq_length 128 \
-#     --seed 128 \
-#     --kshot $SHOT > log.RTE.$SHOT.shot.seed.128.txt 2>&1 &
+    --seed 128 \
+    --kshot $SHOT > log.RTE.$SHOT.shot.seed.128.txt 2>&1 &
