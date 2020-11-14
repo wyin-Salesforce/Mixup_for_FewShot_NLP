8219aa9dcb3219db4fe4b3299e60385c326a61dc
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-14 04:52:21 -0800

    coding

---
+++ FewRel/train.FewRel.batchMixup.pretrain.commands.sh
@@ -0,0 +1,63 @@
+export SHOT=0
+export BATCHSIZE=32
+export EPOCHSIZE=8
+export LEARNINGRATE=1e-5
+export MAXLEN=64
+export BATCHMIXTIMES=400
+
+
+CUDA_VISIBLE_DEVICES=0 python -u train.FewRel.batchMixup.pretrain.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 128 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length $MAXLEN \
+    --seed 42 \
+    --kshot $SHOT \
+    --use_mixup\
+    --batch_mix_times $BATCHMIXTIMES  > log.FewRel.batchMixup.pretrain.$SHOT.shot.seed.42.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=1 python -u train.FewRel.batchMixup.pretrain.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 128 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length $MAXLEN \
+    --seed 16 \
+    --kshot $SHOT \
+    --use_mixup\
+    --batch_mix_times $BATCHMIXTIMES > log.FewRel.batchMixup.pretrain.$SHOT.shot.seed.16.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=2 python -u train.FewRel.batchMixup.pretrain.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 128 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length $MAXLEN \
+    --seed 32 \
+    --kshot $SHOT \
+    --use_mixup\
+    --batch_mix_times $BATCHMIXTIMES > log.FewRel.batchMixup.pretrain.$SHOT.shot.seed.32.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=3 python -u train.FewRel.batchMixup.pretrain.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 128 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length $MAXLEN \
+    --seed 64 \
+    --kshot $SHOT \
+    --use_mixup\
+    --batch_mix_times $BATCHMIXTIMES > log.FewRel.batchMixup.pretrain.$SHOT.shot.seed.64.txt 2>&1 &
