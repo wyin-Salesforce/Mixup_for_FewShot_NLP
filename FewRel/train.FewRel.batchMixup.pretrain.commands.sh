73a7088bddd3261ce7a0874bb3ed02c87a32daf4
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-11-14 05:32:08 -0800

    coding

--- FewRel/train.FewRel.batchMixup.pretrain.commands.sh
+++ FewRel/train.FewRel.batchMixup.pretrain.commands.sh
@@ -1,4 +1,4 @@
-export SHOT=0
+export SHOT=0.75
 export BATCHSIZE=32
 export EPOCHSIZE=8
 export LEARNINGRATE=1e-5
@@ -6,7 +6,7 @@ export MAXLEN=64
 export BATCHMIXTIMES=2400
 
 
-CUDA_VISIBLE_DEVICES=0 python -u train.FewRel.batchMixup.pretrain.py \
+CUDA_VISIBLE_DEVICES=4 python -u train.FewRel.batchMixup.pretrain.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -20,7 +20,7 @@ CUDA_VISIBLE_DEVICES=0 python -u train.FewRel.batchMixup.pretrain.py \
     --use_mixup\
     --batch_mix_times $BATCHMIXTIMES  > log.FewRel.batchMixup.pretrain.$SHOT.shot.seed.42.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python -u train.FewRel.batchMixup.pretrain.py \
+CUDA_VISIBLE_DEVICES=5 python -u train.FewRel.batchMixup.pretrain.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -34,7 +34,7 @@ CUDA_VISIBLE_DEVICES=1 python -u train.FewRel.batchMixup.pretrain.py \
     --use_mixup\
     --batch_mix_times $BATCHMIXTIMES > log.FewRel.batchMixup.pretrain.$SHOT.shot.seed.16.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python -u train.FewRel.batchMixup.pretrain.py \
+CUDA_VISIBLE_DEVICES=6 python -u train.FewRel.batchMixup.pretrain.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
@@ -48,7 +48,7 @@ CUDA_VISIBLE_DEVICES=2 python -u train.FewRel.batchMixup.pretrain.py \
     --use_mixup\
     --batch_mix_times $BATCHMIXTIMES > log.FewRel.batchMixup.pretrain.$SHOT.shot.seed.32.txt 2>&1 &
 
-CUDA_VISIBLE_DEVICES=3 python -u train.FewRel.batchMixup.pretrain.py \
+CUDA_VISIBLE_DEVICES=7 python -u train.FewRel.batchMixup.pretrain.py \
     --task_name rte \
     --do_train \
     --do_lower_case \
